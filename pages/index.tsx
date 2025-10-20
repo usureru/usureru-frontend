485154f5d3449bc4ecd249e44cd7b155707a0545
usureru <usureru2@gmail.com>
2025-10-21 00:00:02 +1300

    Delete pages/index.tsx
    clashing

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding: 20}}>
-      <h1>Home is OK (pages/)</h1>
-      <p>Temporary fallback while we restore App Router.</p>
-    </main>
-  );
-}
